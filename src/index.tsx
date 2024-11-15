cd3ccc8c180aec0b36b4600ca316866313b8d033
Mykola Maik <mykoluamaik@gmail.com>
2024-11-15 17:06:08 +0200

    connect redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import RouterProvider from "@/containers/providers/routerProvider/RouterProvider";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 import "./index.css";
-import RouterProvider from "./containers/providers/routerProvider/RouterProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider />
+    <Provider store={store}>
+      <RouterProvider />
+    </Provider>
   </StrictMode>
 );
